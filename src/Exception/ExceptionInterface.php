46fa9392cb750fdc5ab4ef163a80f58cf9a0b6ab
rw0600 <rw0600@gmail.com>
2017-01-10 17:14:56 +0000

    Add conditions factory and exception package

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,7 @@
+<?php
+namespace Boxspaced\EntityManagerModule\Exception;
+
+interface ExceptionInterface
+{
+
+}
---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,7 @@
+<?php
+namespace Boxspaced\EntityManagerModule\Exception;
+
+class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
+{
+
+}
